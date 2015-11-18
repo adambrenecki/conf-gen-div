390d471d11d5844e73979e6e65f26ec12a8b6157
Andy Marks <amarks@thoughtworks.com>
2015-11-19 07:48:06 +1100

    Had forgotten to load smart-table JS script :-(

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,11 @@
 
 /* Controllers */
 
-var app = angular.module('app', []);
-
-app.controller('ConfListCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('data/confs.json').success(function(data) {
-    $scope.conferences = data;
-  });
-}]);
+angular.module('app', ['smart-table'])
+	.controller('ConfListCtrl', ['$scope', '$http', 
+		function($scope, $http) {
+		  $http.get('data/confs.json').success(function(data) {
+		    $scope.conferences = data;
+		  });
+		}]
+	);
