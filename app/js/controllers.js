5c29dbd852834ca2212c2f97df40067132ec2c8c
Andy Marks <amarks@thoughtworks.com>
2015-11-18 16:56:06 +1100

    Initial commit - based on Angular PhoneCat tutorial app

---
+++ app/js/controllers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Controllers */
+
+var app = angular.module('app', []);
+
+app.controller('ConfListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('data/confs.json').success(function(data) {
+    $scope.conferences = data;
+  });
+
+  $scope.orderProp = '-diversity';
+}]);
