72913b1088b2fab10415cadbfe0d598eb597f3bb
Andy Marks <amarks@thoughtworks.com>
2017-03-20 11:35:56 +1100

    Moved filter tests into their own file

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -28,41 +28,4 @@ describe('Conf List directive', function() {
 
     expect($rootScope.loadedConferences).toEqual([{name: 'Agile Australia'}, {year: '2014'}]);
   });
-
-  describe('helper functions', function() {
-    it('year should provide a friendly version of the conf year', function() {
-      jasmine.clock().mockDate(new Date(2015, 1, 1));
-    	$compile("<conflist></conflist>")($rootScope);
-    	$rootScope.$digest();
-      expect($rootScope.friendlyYear({year: 2015})).toEqual("this year")
-      expect($rootScope.friendlyYear({year: 2014})).toEqual("last year")
-      expect($rootScope.friendlyYear({year: 2013})).toEqual("2 years ago")
-    });
-
-    it('numberOfMen should derive number of men from other fields', function() {
-    	$compile("<conflist></conflist>")($rootScope);
-    	$rootScope.$digest();
-      expect($rootScope.numberOfMen({totalSpeakers: 20, numberOfWomen: 3})).toEqual(17)
-    });
-
-    it('diversityPercentage should derive diversity percentage from other fields', function() {
-    	$compile("<conflist></conflist>")($rootScope);
-    	$rootScope.$digest();
-      expect($rootScope.diversityPercentage({totalSpeakers: 20, numberOfWomen: 3})).toEqual(15)
-    });
-
-    it('diversityScore should score each diversity percentage using school grades', function() {
-    	$compile("<conflist></conflist>")($rootScope);
-    	$rootScope.$digest();
-      expect($rootScope.diversityScore({totalSpeakers: 100, numberOfWomen: 50})).toEqual('A');
-      expect($rootScope.diversityScore({totalSpeakers: 100, numberOfWomen: 49})).toEqual('B');
-      expect($rootScope.diversityScore({totalSpeakers: 100, numberOfWomen: 39})).toEqual('C');
-      expect($rootScope.diversityScore({totalSpeakers: 100, numberOfWomen: 29})).toEqual('D');
-      expect($rootScope.diversityScore({totalSpeakers: 100, numberOfWomen: 19})).toEqual('E');
-      expect($rootScope.diversityScore({totalSpeakers: 100, numberOfWomen: 9})).toEqual('F');
-      expect($rootScope.diversityScore({totalSpeakers: 100, numberOfWomen: 0})).toEqual('F');
-
-    });
-
-  })
 });
--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -1,7 +1,42 @@
-'use strict';
+'use strict'; 
 
-/* jasmine specs for filters go here */
+var filter;
 
-describe('filter', function() {
+describe('The friendlyYear filter', function () {
 
+  beforeEach(function () {
+    module('app');
+	jasmine.clock().mockDate(new Date(2015, 1, 1));
+
+    inject(function ($injector) {
+      filter = $injector.get('$filter')('friendlyYear');
+    });
+  });
+
+  it('should turn the numeric year into a conversation version of itself', function () {
+      expect(filter(2015)).toEqual("this year");
+      expect(filter(2014)).toEqual("last year");
+      expect(filter(2013)).toEqual("2 years ago");
+  });
+});
+
+describe('The scoreClass filter', function () {
+
+  beforeEach(function () {
+    module('app');
+
+    inject(function ($injector) {
+      filter = $injector.get('$filter')('scoreClass');
+    });
+  });
+
+  it('should find the appropriate CSS class for the diversityPercentage', function () {
+      expect(filter(50)).toEqual('percentage-cohort-a');
+      expect(filter(49)).toEqual('percentage-cohort-b');
+      expect(filter(39)).toEqual('percentage-cohort-c');
+      expect(filter(29)).toEqual('percentage-cohort-d');
+      expect(filter(19)).toEqual('percentage-cohort-e');
+      expect(filter(9)).toEqual('percentage-cohort-f');
+      expect(filter(0)).toEqual('percentage-cohort-f');
+  });
 });
